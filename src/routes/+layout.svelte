cea08affd5a32d27ec1d203a1f53ac22fd3cb8b8
Gean Ferreira <gean_roberto@hotmail.com>
2023-06-24 14:11:41 -0300

    feat: Add o layout principal da aplicação

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import '@/assets/scss/index.scss';
+	import Navbar from '@/components/Navbar.svelte';
+</script>
+
+<svelte:head>
+	<title>Gean & Jullyana</title>
+	<meta name="description" content="Site do casal Gean e Jullyana." />
+</svelte:head>
+
+<Navbar />
+<slot />
